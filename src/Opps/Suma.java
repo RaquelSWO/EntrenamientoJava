112a8b66f014116e6bf26dd05bdd0e1629cd19cc
Hernandez <raquel.hernandez@softwareone.com>
2025-12-12 10:54:25 -0500

    decimo comit 12-11-2025 - se incluyen las clases Suma y HijaDeSuma

---
+++ src/Opps/Suma.java
@@ -0,0 +1,20 @@
+package Opps;
+
+public class Suma {
+	public void Sumar (int a, int b)
+	{
+		int c=a+b;
+		System.out.println("La suma es:"+c);
+	}
+	
+	public void Sumar (int a, int b, int c)
+	{
+	 int d= a+b+c;
+	System.out.println("La suma es:"+d);
+	}
+	 public  double Sumar (double  a, double  b, double c)
+	 {
+		 double d=a+b+c;
+		 return d;
+	 }
+}
